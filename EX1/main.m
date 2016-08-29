eec1d875e6d0b11094fb15a5c0cb837892f3e5f2
icelucio <lucio@lucios-mac.local>
2016-08-29 16:16:55 -0700

    Initial Commit

---
+++ EX1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EX1
+//
+//  Created by Lucio on 8/29/16.
+//  Copyright © 2016 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
